a8eb89347c0f1d585142a6eb3087fdb197706b27
denglitong <litong.deng@foxmail.com>
2021-10-24 23:35:25 +0800

    Enable Eureka Server
    Signed-off-by: denglitong <litong.deng@foxmail.com>

---
+++ src/main/java/com/denglitong/eurekaservice/Application.java
@@ -0,0 +1,17 @@
+package com.denglitong.eurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author litong.deng@foxmail.com
+ * @date 2021/10/24
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
